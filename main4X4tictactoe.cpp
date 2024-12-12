5d37b8d9f6f1f20446247c8cf47d99f896df4a17
MarwanTamerSayed <20230380@stud.fci-cu.edu.eg>
2024-12-12 14:07:40 +0200

    Update main4X4tictactoe.cpp
    I change the cout to be by letters 
    random player constructor takes the board

--- main4X4tictactoe.cpp
+++ main4X4tictactoe.cpp
@@ -1,12 +1,4 @@
-#include <iostream>
-#include "BoardGame_Classes.h"
-#include "4X4_TICTACTOE.h"
-#include "MinMaxPlayer.h"
-
-using namespace std;
-
-int main() {
-    char choice;
+char choice;
     Player<char>* players[2];
     X_O_Board<char>* B = new X_O_Board<char>();
     string player1Name, player2Name;
@@ -17,46 +9,44 @@ int main() {
     cout << "Enter Player X name: ";
     cin >> player1Name;
     cout << "Choose Player X type:\n";
-    cout << "1. Human\n";
-    cout << "2. Random Computer\n";
-    cout << "3. Smart Computer (AI)\n";
+    cout << "a. Human\n";
+    cout << "b. Random Computer\n";
+    cout << "c. Smart Computer (AI)\n";
     cout << "Enter Choice: ";
     cin >> choice;
 
     if (choice == 'a') {
         players[0] = new X_O_Human_Player<char>(player1Name, 'X');
     } else if (choice == 'b') {
-        players[0] = new X_O_Game_Random_Player<char>('X');
+        players[0] = new X_O_Game_Random_Player<char>('X',B);
         players[0]->setBoard(B);
     } else if (choice == 'c') {
-        players[0] = new X_O_MinMax_Player<char>('X');
+
         players[0]->setBoard(B);
     } else {
         cout << "Invalid choice for Player X. Exiting the game.\n";
-        return 1;
     }
 
     // Set up Player 2
     cout << "Enter Player O name: ";
     cin >> player2Name;
     cout << "Choose Player O type:\n";
-    cout << "1. Human\n";
-    cout << "2. Random Computer\n";
-    cout << "3. Smart Computer (AI)\n";
+    cout << "a. Human\n";
+    cout << "b. Random Computer\n";
+    cout << "c. Smart Computer (AI)\n";
     cout << "Enter Choice: ";
     cin >> choice;
 
     if (choice == 'a') {
         players[1] = new X_O_Human_Player<char>(player2Name, 'O');
     } else if (choice == 'b') {
-        players[1] = new X_O_Game_Random_Player<char>('O');
+        players[1] = new X_O_Game_Random_Player<char>('O',B);
         players[1]->setBoard(B);
     } else if (choice == 'c') {
-        players[1] = new X_O_MinMax_Player<char>('O');
+
         players[1]->setBoard(B);
     } else {
         cout << "Invalid choice for Player O. Exiting the game.\n";
-        return 1;
     }
 
     // Create the game manager and run the game
@@ -68,6 +58,3 @@ int main() {
     for (int i = 0; i < 2; ++i) {
         delete players[i];
     }
-
-    return 0;
-}
