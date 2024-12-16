073db8812db6815491e6b60262adc1ad79d037c2
RanaTarekAhmed <161890296+RanaTarekAhmed@users.noreply.github.com>
2024-12-16 09:38:43 +0200

    Update 3x3misereX_O.h

--- Rana/3x3misereX_O.h
+++ Rana/3x3misereX_O.h
@@ -3,7 +3,8 @@
 using namespace std;
 
 template <typename T>
-class X_O_Board_misere:public Board<T> {
+class X_O_Board_misere:public Board<T>
+{
 public:
     X_O_Board_misere ();
     bool update_board (int x , int y , T symbol);
@@ -14,29 +15,34 @@ public:
 };
 
 template <typename T>
-class X_O_Player_misere : public Player<T> {
+class X_O_Player_misere : public Player<T>
+{
 public:
     X_O_Player_misere (string name, T symbol);
     void getmove(int& x, int& y) ;
 };
 
 template <typename T>
-class X_O_Random_Player_misere : public RandomPlayer<T>{
+class X_O_Random_Player_misere : public RandomPlayer<T>
+{
 public:
     X_O_Random_Player_misere (T symbol);
     void getmove(int &x, int &y) ;
 };
 
-//IMPLEMENTATION
+//############################# IMPLEMENTATION ###############################################################3
 
-// Constructor for X_O_Board
+// Constructor for X_O_Board_misere
 template <typename T>
-X_O_Board_misere<T>::X_O_Board_misere() {
+X_O_Board_misere<T>::X_O_Board_misere()
+{
     this->rows = this->columns = 3;
     this->board = new char*[this->rows];
-    for (int i = 0; i < this->rows; i++) {
+    for (int i = 0; i < this->rows; i++)
+    {
         this->board[i] = new char[this->columns];
-        for (int j = 0; j < this->columns; j++) {
+        for (int j = 0; j < this->columns; j++)
+        {
             this->board[i][j] = 0;
         }
     }
@@ -44,14 +50,18 @@ X_O_Board_misere<T>::X_O_Board_misere() {
 }
 
 template <typename T>
-bool X_O_Board_misere<T>::update_board(int x, int y, T mark) {
-    // Only update if move is valid
-    if (!(x < 0 || x >= this->rows || y < 0 || y >= this->columns) && (this->board[x][y] == 0|| mark == 0)) {
-        if (mark == 0){
+bool X_O_Board_misere<T>::update_board(int x, int y, T mark)
+{
+    //update if move is valid
+    if (!(x < 0 || x >= this->rows || y < 0 || y >= this->columns) && (this->board[x][y] == 0|| mark == 0))
+    {
+        if (mark == 0)
+        {
             this->n_moves--;
             this->board[x][y] = 0;
         }
-        else {
+        else
+        {
             this->n_moves++;
             this->board[x][y] = toupper(mark);
         }
@@ -62,10 +72,13 @@ bool X_O_Board_misere<T>::update_board(int x, int y, T mark) {
 
 // Display the board and the pieces on it
 template <typename T>
-void X_O_Board_misere<T>::display_board() {
-    for (int i = 0; i < this->rows; i++) {
+void X_O_Board_misere<T>::display_board()
+{
+    for (int i = 0; i < this->rows; i++)
+    {
         cout << "\n| ";
-        for (int j = 0; j < this->columns; j++) {
+        for (int j = 0; j < this->columns; j++)
+        {
             cout << "(" << i << "," << j << ")";
             cout << setw(2) << this->board[i][j] << " |";
         }
@@ -74,19 +87,23 @@ void X_O_Board_misere<T>::display_board() {
     cout << endl;
 }
 
-// Returns true if there is any winner
+
 template <typename T>
-bool X_O_Board_misere<T>::is_win() {
+bool X_O_Board_misere<T>::is_win()
+{
     // Check rows and columns
-    for (int i = 0; i < this->rows; i++) {
+    for (int i = 0; i < this->rows; i++)
+        {
         if ((this->board[i][0] == this->board[i][1] && this->board[i][1] == this->board[i][2] && this->board[i][0] != 0) ||
-            (this->board[0][i] == this->board[1][i] && this->board[1][i] == this->board[2][i] && this->board[0][i] != 0)) {
+            (this->board[0][i] == this->board[1][i] && this->board[1][i] == this->board[2][i] && this->board[0][i] != 0))
+        {
             return true;
         }
     }
     // Check diagonals
     if ((this->board[0][0] == this->board[1][1] && this->board[1][1] == this->board[2][2] && this->board[0][0] != 0) ||
-        (this->board[0][2] == this->board[1][1] && this->board[1][1] == this->board[2][0] && this->board[0][2] != 0)) {
+        (this->board[0][2] == this->board[1][1] && this->board[1][1] == this->board[2][0] && this->board[0][2] != 0))
+    {
         return true;
     }
     return false;
@@ -94,12 +111,14 @@ bool X_O_Board_misere<T>::is_win() {
 
 // Return true if 9 moves are done and no winner
 template <typename T>
-bool X_O_Board_misere<T>::is_draw() {
+bool X_O_Board_misere<T>::is_draw()
+{
     return (this->n_moves == 9 && !is_win());
 }
 
 template <typename T>
-bool X_O_Board_misere<T>::game_is_over() {
+bool X_O_Board_misere<T>::game_is_over()
+{
     return is_win() || is_draw();
 }
 
@@ -108,43 +127,51 @@ template <typename T>
 X_O_Player_misere<T>::X_O_Player_misere(string name, T symbol) : Player<T>(name, symbol) {}
 
 template <typename T>
-void X_O_Player_misere<T>::getmove(int& x, int& y) {
-    while (true) {
-        cout << "\nPlease enter your index move row and column (0 to 2) separated by a space:";
+void X_O_Player_misere<T>::getmove(int& x, int& y)
+{
+    while (true)
+    {
+        cout << "\n>>Please enter your index move row and column (0 to 2) separated by a space:";
         string x1, y1;
         cin >> x1 >> y1;
         bool validInput = true;
 
-        if (x1 == "0" || x1 == "1" || x1 == "2") {
+        if (x1 == "0" || x1 == "1" || x1 == "2")
+        {
             x = stoi(x1);
-        } else {
+        }
+        else
+        {
             cout << "Invalid index move row!" << endl;
             validInput = false;
         }
-
-        if (y1 == "0" || y1 == "1" || y1 == "2") {
+        if (y1 == "0" || y1 == "1" || y1 == "2")
+        {
             y = stoi(y1);
-        } else {
+        }
+        else
+        {
             cout << "Invalid index move column!" << endl;
             validInput = false;
         }
-
         if (validInput) {
             break;
         }
     }
 }
 
-// Constructor for X_O_Random_Player
+// Constructor for X_O_Random_Player_misere
 template <typename T>
-X_O_Random_Player_misere<T>::X_O_Random_Player_misere(T symbol) : RandomPlayer<T>(symbol) {
+X_O_Random_Player_misere<T>::X_O_Random_Player_misere(T symbol) : RandomPlayer<T>(symbol)
+{
     this->dimension = 3;
     this->name = "Random Computer Player";
-    srand(static_cast<unsigned int>(time(0)));  // Seed the random number generator
+    srand(static_cast<unsigned int>(time(0)));
 }
 
 template <typename T>
-void X_O_Random_Player_misere<T>::getmove(int& x, int& y) {
+void X_O_Random_Player_misere<T>::getmove(int& x, int& y)
+{
     x = rand() % this->dimension;  // Random number between 0 and 2
     y = rand() % this->dimension;
 }
